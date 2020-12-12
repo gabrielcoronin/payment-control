92372f8788eca0735c745e26cb59abab9a0e5c74
Gabriel Coronin <gabrielcoronin@gmail.com>
2020-12-12 14:31:35 -0300

    feat(Details): inclusão do Details no sellers

--- PaymentControl/Services/SellerService.cs
+++ PaymentControl/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using PaymentControl.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentControl.Data;
 using PaymentControl.Models;
 using System;
 using System.Collections.Generic;
@@ -29,7 +30,7 @@ namespace PaymentControl.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(x => x.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
