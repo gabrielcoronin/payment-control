0cb9a4a5ec506d18476e7e4f77cfd3acf75d5c36
Gabriel Coronin <gabrielcoronin@gmail.com>
2020-12-12 00:03:06 -0300

    feat(controllers e entidades): inclusão de novas controllers e entidades

--- PaymentControl/Data/PaymentControlContext.cs
+++ PaymentControl/Data/PaymentControlContext.cs
@@ -14,6 +14,8 @@ namespace PaymentControl.Data
         {
         }
 
-        public DbSet<PaymentControl.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
--- PaymentControl/Migrations/20201212030200_OtherEntities.Designer.cs
+++ PaymentControl/Migrations/20201212030200_OtherEntities.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PaymentControl.Data;
 
 namespace PaymentControl.Migrations
 {
     [DbContext(typeof(PaymentControlContext))]
-    partial class PaymentControlContextModelSnapshot : ModelSnapshot
+    [Migration("20201212030200_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
