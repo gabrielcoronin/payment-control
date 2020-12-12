dbe327193bc8d2b7a0ece296e7e5d4c2884b8fac
Gabriel Coronin <gabrielcoronin@gmail.com>
2020-12-12 15:05:56 -0300

    refactor(datas): localização e formatação das datas

--- PaymentControl/Startup.cs
+++ PaymentControl/Startup.cs
@@ -11,6 +11,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using PaymentControl.Data;
 using PaymentControl.Services;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 
 namespace PaymentControl
 {
@@ -42,6 +44,16 @@ namespace PaymentControl
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
